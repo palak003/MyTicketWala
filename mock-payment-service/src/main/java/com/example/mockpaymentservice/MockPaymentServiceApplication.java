45519684c206aae5b61e8cff3dd8c3ce8877dd6b
palak003 <goelpalak003@gmail.com>
2023-05-13 12:46:20 +0530

    feat: Initialized 4 microservices

---
+++ mock-payment-service/src/main/java/com/example/mockpaymentservice/MockPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.mockpaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockPaymentServiceApplication.class, args);
+	}
+
+}
---
+++ mock-payment-service/src/test/java/com/example/mockpaymentservice/MockPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mockpaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
